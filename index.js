b51cc1bedd5529f217c209d1057462fa28f74051
Amaurys Mendez <amardemir92@gmail.com>
2022-11-20 20:55:21 -0400

    commit tree ba18774b45dbe7f28797c6bd6b8f4490bd46ef7b

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a ="500", b="253"){
+    return a - b;
+}
+
+function multiply(a="2216.52", b="50"){
+    return a * b;
+}
+
+function divide(a, b){
+    return a / b;
+}
+
+function increment(n){
+    return ++n;
+}
+
+function decrement(n){
+    return --n;
+}
+
+function makeInt(n){
+    parseInt('n',10)
+    return parseFloat(n)
+}
+
+function preserveDecimal(n){
+    parseInt ('n',10)
+    return parseFloat(n)
+}
